9c3eddd27449fc40ab7ac5ecb1742141e93ee113
Carlos L. Torres <carlos.torres@rackspace.com>
2014-10-11 18:20:13 -0500

    Solved exercise 3.5

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -64,7 +64,10 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(_, xs) => drop(xs, n - 1)
     }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h,t) => if (f(h)) dropWhile(t, f) else l
+  }
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
