3fd3a08ba60294bc8db2e38e800cc4197fd16ab5
Carlos L. Torres <carlos.torres@rackspace.com>
2014-10-11 17:23:23 -0500

    Solved exercise 3.1

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -26,7 +26,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
     case Cons(h, t) => h + sum(t)
     case _ => 101 
-  }
+  } // == 3
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
