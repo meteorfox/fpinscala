52c46a91f455373acba60458fe3e9b4bb27c0b66
Carlos L. Torres <carlos.torres@rackspace.com>
2014-10-11 14:57:04 -0500

    Solved exercises 2.3, 2.4 and 2.5

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -142,8 +142,8 @@ object PolymorphicFunctions {
       if (n >= (as.length - 1)) stillSorted
       else if (!stillSorted) false
       else loop(n + 1, gt(as(n), as(n+1)))
-    
-    loop(0, true)
+    if (as.length == 0) true
+    else loop(0, true)
   }
 
   // Polymorphic functions are often so constrained by their type
@@ -156,14 +156,14 @@ object PolymorphicFunctions {
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = 
+    a => (b => f(a, b))
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a, b) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -177,6 +177,6 @@ object PolymorphicFunctions {
 
   // Exercise 5: Implement `compose`
 
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+  def compose[A,B,C](f: B => C, g: A => B): A => C = 
+    a => f(g(a))
 }
